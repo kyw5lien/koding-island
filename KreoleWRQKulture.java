e2024e7c2899e91fe1148d14f9533c920ad2a6c7
Dario Neves <100414412+kyw5lien@users.noreply.github.com>
2023-02-19 03:46:46 +0000

    Create KreoleWRQKulture.java
    Create KreoleWRQKulture: a Java Class representation. #notes -> imported packages might be requested, made available, OR implemented from scratch #jdk

---
+++ KreoleWRQKulture.java
@@ -0,0 +1,19 @@
+class KreolePiece{
+public static String printConvertedPieceChar(int I){
+	int piece = (char) I;
+	++piece;
+	piece++;
+	System.out.println((double) I /*TODO CODE goes here ...*/ /*END of TODO CODE goes here ...*/);
+	return piece;
+}
+}
+
+public class KreoleWRQKulture{
+public static void main(String[] args){
+int piece = (char) 'i' / ('~' +'=');
+--piece;
+piece--;
+piece += (float)'i' /* ALT TODO CODE goes here ...*/ /* END of ALT TODO CODE goes here ...*/; 
+System.out.println(KreolePiece.printConvertedPieceChar(piece));
+}
+}
