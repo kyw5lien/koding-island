33b1ee37465d2cfbeae2c6826da75daad511cc58
Dario Neves <100414412+kyw5lien@users.noreply.github.com>
2023-02-22 10:59:54 +0000

    Update KreoleWRQKulture.java: identing code + updating imported package, and Class
    Update KreoleWRQKulture.java: identing code + updating imported package, and Class.

--- KreoleWRQKulture.java
+++ KreoleWRQKulture.java
@@ -1,21 +1,20 @@
-import kreole.sdg.cs.fibonnaci.sequencer;
+import kreole.sdg.cs.fibonnaci.Sequencer;
 
 class KreolePiece{
-public static String printConvertedPieceChar(int I){
-	int piece = (char) I;
-	++piece;
-	piece++;
-	System.out.println((double) I /*TODO CODE goes here ...*/ /*END of TODO CODE goes here ...*/);
-	return piece;
+	public static String printConvertedPieceChar(int I){
+		int piece = (char) I;
+		++piece;
+		piece++;
+		System.out.println((double) I /*TODO CODE goes here ...*/ /*END of TODO CODE goes here ...*/);
+		return piece;
+	}
 }
-}
-
 public class KreoleWRQKulture{
-public static void main(String[] args){
-int piece = (char) 'i' / ('~' +'=');
---piece;
-piece--;
-piece += (float)'i' /* ALT TODO CODE goes here ...*/ /* END of ALT TODO CODE goes here ...*/; 
-System.out.println(KreolePiece.printConvertedPieceChar(piece));
-}
+	public static void main(String[] args){
+		int piece = (char) 'i' / ('~' +'=');
+		--piece;
+		piece--;
+		piece += (float) 'i' /* ALT TODO CODE goes here ...*/ /* END of ALT TODO CODE goes here ...*/; 
+		System.out.println(KreolePiece.printConvertedPieceChar(piece));
+	}
 }
